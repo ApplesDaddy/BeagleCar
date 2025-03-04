ed22ff9cc7ecc4d49729637239a77b3fe56c7cbd
Bogdan <bogdan26bogdan14@gmail.com>
2025-03-04 12:17:05 -0800

    Basic file format, and asio / crow.hpp included

---
+++ build/testing/CMakeFiles/webServerTest.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for webServerTest.
+# This may be replaced when dependencies are built.
---
+++ build/testing/CMakeFiles/webServerTest.dir/src/webServerTest.c.o.d
@@ -0,0 +1,3 @@
+testing/CMakeFiles/webServerTest.dir/src/webServerTest.c.o: \
+ /home/bogdan26bogdan14/BeagleCar/testing/src/webServerTest.c \
+ /usr/include/stdc-predef.h
