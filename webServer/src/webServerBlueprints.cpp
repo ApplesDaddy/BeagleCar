f89d0701ef70ef1a2c52685e60cc17614376200e
Bogdan <49450616+Bogdan26Bogdan@users.noreply.github.com>
2025-03-05 13:45:34 -0800

    template examples

--- webServer/src/webServerBlueprints.cpp
+++ webServer/src/webServerBlueprints.cpp
@@ -3,6 +3,7 @@
 
 // Example from: https://crowcpp.org/master/getting_started/your_first_application/
 void add_routes(crow::SimpleApp& app){
+    
     //define your endpoint at the root directory
     CROW_ROUTE(app, "/")([](){
         return "Hello world";
@@ -17,6 +18,25 @@ void add_routes(crow::SimpleApp& app){
 
         return page.render(ctx);
     });
+
+
+    //Return a template from a file with values
+    CROW_ROUTE(app, "/template_file/<int>")([](int count){
+        auto page = crow::mustache::load("template_page.html");
+        crow::mustache::context ctx;
+        ctx["value"] = count;
+
+        return page.render(ctx);
+    });
+
+    //Return a template from a file
+    CROW_ROUTE(app, "/load_file")([](){
+        auto page = crow::mustache::load_text("template_page.html");
+        
+        return page.render();
+    });
+
+    //TODO: Websocket example
 
 
 }
