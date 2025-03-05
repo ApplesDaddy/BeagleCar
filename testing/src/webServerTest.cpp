4bc6667b1e946a1e77646af3dd773c17994e7e6b
Bogdan <bogdan26bogdan14@gmail.com>
2025-03-04 21:43:30 -0800

    Moved sutff around and building works

---
+++ testing/src/webServerTest.cpp
@@ -0,0 +1,13 @@
+#include "webServer/webServerBlueprints.h"
+#include "crow_all.h"
+
+
+int main()
+{
+    crow::SimpleApp app; //define your crow application
+
+    add_routes(app); //add the routes to the app
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.port(18080).multithreaded().run();
+}
--- webServer/src/webServerBlueprints.cpp
+++ webServer/src/webServerBlueprints.cpp
@@ -1,1 +1,10 @@
 #include "crow_all.h"
+#include "webServer/webServerBlueprints.h"
+
+// Example from: https://crowcpp.org/master/getting_started/your_first_application/
+void add_routes(crow::SimpleApp& app){
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+}
