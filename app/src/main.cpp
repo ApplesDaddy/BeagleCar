e0a50da17e800d511586115be3e153d6bcfa079a
J Lucas <nal9@sfu.ca>
2025-03-06 12:44:33 -0800

    bug fixes

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -3,23 +3,31 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include <libavcodec/avcodec.h>
-#include <libavformat/avformat.h>
+extern "C" {
+#include "libavcodec/avcodec.h"
+#include "libavformat/avformat.h"
 
 #include "hal/lcd.h"
-
+}
 #define INBUF_SIZE 4096
 
 
 // source: https://ffmpeg.org/doxygen/4.2/decode_video_8c-example.html
-static void decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt)
+static void decode(AVCodecContext *dec_ctx, AVFrame *frame, AVPacket *pkt, int stream_idx)
 {
+    if (pkt->stream_index != stream_idx){ return; }
     int ret = avcodec_send_packet(dec_ctx, pkt);
+    // int got_pic_ptr;
+    // int ret = avcodec_decode_video2(dec_ctx, frame, &got_pic_ptr, pkt);
     if (ret < 0)
     {
-        fprintf(stderr, "Error sending a packet for decoding\n");
-        exit(1);
+        fprintf(stderr, "Error sending a packet for decoding %d\n", ret);
+        // exit(1);
+        return;
     }
+    // P frame or incomplete frame
+    // if(got_pic_ptr < 0)
+    // { return; }
 
     while (ret >= 0)
     {
@@ -54,7 +62,7 @@ int main()
     memset(inbuf + INBUF_SIZE, 0, AV_INPUT_BUFFER_PADDING_SIZE);
 
     /* find the MPEG-4 decoder */
-    const AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_MPEG4);
+    const AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_H264);
     if (!codec)
     {
         fprintf(stderr, "Codec not found\n");
@@ -87,21 +95,25 @@ int main()
         fprintf(stderr, "Could not get stream info\n");
         exit(1);
     }
-    AVCodec * video_dec = (AVCodec*)1;
-    int video_stream_index = av_find_best_stream( format, AVMEDIA_TYPE_VIDEO, -1, -1, & video_dec, 0 );
+    const AVCodec * video_dec = (AVCodec*)1;
+    int video_stream_index = av_find_best_stream( format, AVMEDIA_TYPE_VIDEO, -1, -1, & codec, 0 );
     if ( video_stream_index < 0 )
     {
         fprintf(stderr, "Could not get stream index\n");
         exit(1);
     }
     AVStream* videoStream = format->streams[ video_stream_index ];
+    AVCodecParameters *codecpar = videoStream->codecpar;
     double FPS = (double)videoStream->r_frame_rate.num / (double)videoStream->r_frame_rate.den;
 
     /* For some codecs, such as msmpeg4 and mpeg4, width and height
        MUST be initialized there because this information is not
        available in the bitstream. */
-    c->height = 540;
-    c->width = 960;
+    // c->height = codecpar->height;
+    // c->width = codecpar->width;
+    // c->pix_fmt = AV_PIX_FMT_YUV420P;
+    // c->framerate = format->streams[video_stream_index]->r_frame_rate;
+    avcodec_parameters_to_context(c, codecpar);
 
     /* open it */
     if (avcodec_open2(c, codec, NULL) < 0)
@@ -146,13 +158,13 @@ int main()
             data_size -= ret;
 
             // parse into AVFrame
-            decode(c, frame, pkt);
+            decode(c, frame, pkt, video_stream_index);
             lcd_show_frame(frame);
         }
     }
 
     /* flush the decoder */
-    decode(c, frame, NULL);
+    decode(c, frame, NULL, video_stream_index);
 
     fclose(f);
 
